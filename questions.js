45809fcfa48a4a21e948cee2ed59d73152bdb897
Craig Whippo <CraigWhippo@users.noreply.github.com>
2019-01-02 14:16:57 -0700

    Update questions.js

--- questions.js
+++ questions.js
@@ -2,7 +2,22 @@ const questions = {
   'Q01': {
     'type': 'highlight',
     'title': 'Motive',
-    'Question': 'Highlight the resons why additiional research is needed',
+    'Question': 'Highlight the statements describing reasons why additional research is needed',
+    'anchored': true,
+    'repeatable': true,
+  },
+    'Q02': {
+    'type': 'highlight',
+    'title': 'Objective',
+    'Question': 'Highlight the statements describing what the authors wanted to know',
+    'anchored': true,
+    'repeatable': true,
+  },
+    },
+    'Q03': {
+    'type': 'highlight',
+    'title': 'General Approach',
+    'Question': 'Highlight the statements describing what the authors general approach,
     'anchored': true,
     'repeatable': true,
   },
